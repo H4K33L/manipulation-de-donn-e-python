63e996c5855db1904ded9255c896e4f9da2ef9f6
lemadnighofficiel <matthieu.caron25@gmail.com>
2025-03-11 14:37:56 +0100

    added sums and averages with survived

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-import sys
-print("Version Python :", sys.version)
 import pandas as pd
-print("Version pandas :", pd.__version__)
-print("Installation OK, bon démarrage !")
+df = pd.read_csv("titanic/train.csv")
+
+sum_dead = (df["Survived"] == 0).sum()
+sum_alive = (df["Survived"] == 1).sum()
+
+print(f"Nombre de personnes décédées : {sum_dead}")
+print(f"Nombre de survivants : {sum_alive}")
+
+av_dead = (df["Survived"] == 0).mean()
+av_alive = (df["Survived"] == 1).mean()
+
+print(f"Moyenne (proportion) de personnes décédées : {av_dead:.2%}")
+print(f"Moyenne (proportion) de personnes en vie : {av_alive:.2%}")
