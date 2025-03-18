f0ce2817c6e9d103fd482d67365dcb690d550509
Cy4n1 <156065290+Cassian-J@users.noreply.github.com>
2025-03-18 14:54:51 +0100

    report html

--- tab.py
+++ tab.py
@@ -8,12 +8,10 @@ print("Installation OK, let's start!")
 df = pd.read_csv("titanic/train.csv")
 df["Has_Cabin"] = df["Cabin"].notna()
 
-# Définition des groupes d'âge
 bins = [0, 12, 17, 60, float('inf')]
 labels = ['Children (0-12)', 'Teenagers (13-17)', 'Adults (18-60)', 'Seniors (60+)']
 df['AgeGroup'] = pd.cut(df['Age'], bins=bins, labels=labels)
 
-# Création d'une table croisée
 pivot_table = df.pivot_table(
     values='PassengerId', 
     index=['Pclass', 'Sex', 'AgeGroup', 'Has_Cabin'], 
@@ -22,14 +20,10 @@ pivot_table = df.pivot_table(
     fill_value=0
 )
 
-# Renommage des colonnes pour plus de clarté
 pivot_table.columns = ['Died', 'Survived']
 pivot_table['Total'] = pivot_table['Died'] + pivot_table['Survived']
 pivot_table['Survival Rate (%)'] = (pivot_table['Survived'] / pivot_table['Total']) * 100
 
-# Sauvegarde du tableau croisé en fichier texte avec encodage UTF-8
 with open("survival_stats_table.txt", "w", encoding="utf-8") as f:
     f.write(pivot_table.to_string())
 
-# Affichage du tableau dans la console
-print(pivot_table)
