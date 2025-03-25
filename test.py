37a606796a617b4da8f40c209ad2fa99136adcc8
Cy4n1 <156065290+Cassian-J@users.noreply.github.com>
2025-03-25 15:48:01 +0100

    modele accurracy 87%

--- test.py
+++ test.py
@@ -1,27 +1,23 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
 
 def preprocess_data(filepath, imputer=None):
     df = pd.read_csv(filepath)
-    
     if imputer is None:
         imputer = SimpleImputer(strategy="mean")
         df[['Age', 'Fare']] = imputer.fit_transform(df[['Age', 'Fare']])
     else:
         df[['Age', 'Fare']] = imputer.transform(df[['Age', 'Fare']])
-    
     df['isMale'] = df["Sex"].map({'male': 1, 'female': 0})
-    
     df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
-    
     df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin'], axis=1)
-    
     df = pd.get_dummies(df, columns=['Embarked'], drop_first=True)
     
     return df
@@ -29,7 +25,6 @@ def preprocess_data(filepath, imputer=None):
 def train_and_evaluate(df, model_type="random_forest"):
     X = df.drop('Survived', axis=1)
     y = df['Survived']
-    
     X_train, _, y_train, _ = train_test_split(X, y, test_size=None, random_state=42)
     X_test = preprocess_data("titanic/test.csv")
     y_test = pd.read_csv("titanic/gender_submission.csv")['Survived']
@@ -37,13 +32,11 @@ def train_and_evaluate(df, model_type="random_forest"):
         model = LogisticRegression(max_iter=1000)
     elif model_type == "random_forest":
         model = RandomForestClassifier(n_estimators=100, random_state=42)
-    
+
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-    
     model.fit(X_train_scaled, y_train)
-    
     y_pred = model.predict(X_test_scaled)
 
     return y_test, y_pred
