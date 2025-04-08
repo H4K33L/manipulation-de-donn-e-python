9bacbb15b9192b11571be232d9ead1d6e0e05416
kelyan danis <kelyan.danis@ynov.com>
2025-04-08 13:40:37 +0200

    add test base

---
+++ tests/test_model.py
@@ -0,0 +1,8 @@
+def test_train () :
+    assert()
+
+def test_predict () :
+    assert()
+
+def test_evaluate () :
+    assert()
---
+++ tests/test_preprocessor.py
@@ -0,0 +1,8 @@
+def test_fit () :
+    assert()
+
+def test_transform () :
+    assert()
+
+def test_fit_transform () :
+    assert()
